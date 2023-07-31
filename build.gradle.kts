0175c41de227dc227abcadb56617a25311f1b8ef
Qboi123 <qboiwastaken@gmail.com>
2023-07-31 13:47:57 +0200

    Update icon and Kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.9.0"
     id("java")
     id("org.panteleyev.jpackageplugin") version "1.5.0"
 }
